000e9d9195adf657d2d791a6c80f76cee3628afc
Yining Wang <yining.wang@utexas.edu>
2020-01-09 08:30:57 -0600

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,14 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from "react";
+
+export default class CoordinateButton extends Component {
+  getXY = e => [e.clientX, e.clientY];
+
+  render() {
+    return (
+      <button
+        onClick={e => this.props.onReceiveCoordinates(this.getXY(e))}
+      ></button>
+    );
+  }
+}
